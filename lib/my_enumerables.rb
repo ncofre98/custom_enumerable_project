156cc3a190037246232a971c910d21a9f063af51
Nestor Cofre <50756891+ncofre98@users.noreply.github.com>
2025-05-08 16:06:24 -0600

    Fixed! :)

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,22 +1,22 @@
 module IndexLoopHelper
   def each_index_yielding
     for i in 0...self.size
-      yield(self[i])
+      yield(i)
     end
   end
 end
 
 module Enumerable
-  def my_each_with_index
-    
-  end
 end
 
 class Array
   include IndexLoopHelper
 
   def my_each
-    each_index_yielding { |i| yield(i) }
+    each_index_yielding { |i| yield(self[i]) }
     self
   end
 end
+
+#[42, 69, 1024].each_with_index { |e, i| puts "Position: #{i}, Value: #{e}" }
+# expected: Position: 0, Value: 42 ... etc
